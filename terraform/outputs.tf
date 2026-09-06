eafa30ee9c0ac15d1a1592785eb7985d7d439217
Lazar Grbic <grbiclazar18@gmail.com>
2026-09-06 23:54:00 +0200

    updated outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,12 +4,6 @@ output "gke_cluster_endpoint" {
   value       = google_container_cluster.sre_challenge_cluster.endpoint
 }
 
-# The Primary CIDR Block of the Subnet
-output "subnet_primary_cidr" {
-  description = "The primary IP address range of the GKE subnet."
-  value       = google_compute_subnetwork.sre_challenge_subnet.ip_cidr_range
-}
-
 output "repository_url" {
   value       = "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.sre_challenge_repo.repository_id}"
   description = "The URL for the Docker image repository"
